7b80537f0298147cb79b5ff09bdb4337725b38c7
Tigran-studio <tigran.gasparyan.dev@gmail.com>
2024-03-18 22:07:51 +0400

    Initial commit

---
+++ src/main/java/com/example/veolia/VeoliaApplication.java
@@ -0,0 +1,11 @@
+package com.example.veolia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeoliaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VeoliaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/veolia/VeoliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.veolia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeoliaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
